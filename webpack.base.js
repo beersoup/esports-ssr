2a78f8fdab098ae34500a88f6a0590f3058b5540
Jennie Lenier <beersoup@hotmail.com>
2018-04-22 22:30:59 +0200

    Config

--- webpack.base.js
+++ webpack.base.js
@@ -12,11 +12,7 @@ module.exports = {
                         ['env', { targets: { browsers: ['last 2 versions'] } }]
                     ]
                 }
-            },
-            { test: /\.(png|jpg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,  loader: "file?name=img/[name].[ext]" },
-            { test: /\.(woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,  loader: "file?name=fonts/[name].[ext]" },
-            { test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    loader: "file?name=fonts/[name].[ext]" },
-            { test: /\.svg(\?v=[0-9]\.[0-9]\.[0-9])?$/,    loader: "file-loader" }
+            }
         ]
     }
 }
